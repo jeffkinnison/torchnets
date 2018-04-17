08f7eabdd9a5cb7d519ceb85cb0fedcb80c0a7e9
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-04-16 19:08:10 -0700

    setup.py to install torchnets

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='torchnets',
+    version='0.1a1',
+    description='Implementations of useful neural nets in pytorch',
+    url='https://github.com/jeffkinnison/pyrameter',
+    author='Jeff Kinnison',
+    author_email='jkinniso@nd.edu',
+    packages=['torchnets',
+              'torchnets.inception',
+              'torchnets.lenet',
+              'torchnets.resnets',
+              'torchnets.unet',
+              'torchnets.vgg'
+              'torchnets.utils',],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Users',
+        'License :: MIT',
+        'Topic :: Machine Learning :: Neural Networks',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+    ],
+    keywords='machine_learning neural_networks',
+    install_requires=[
+        'torch>=0.3.0',
+        'torchvision'
+    ],
+)
