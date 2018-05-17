2339c33b4fc8c1afbf5247de2690df3097028569
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-05-17 13:47:36 -0400

    add gaussian noise transform

--- torchnets/loaders/transforms.py
+++ torchnets/loaders/transforms.py
@@ -39,3 +39,41 @@ def whiten(x, e):
     sigma = x * x.t / x.shape[1]
     u, s, _ = torch.svd(sigma, some=False)
     return u * torch.diag(1 / torch.sqrt(S.diag() + e)) * u.t * x
+
+
+class GaussianNoise(object):
+    """Apply Gaussian noise to an image.
+
+    Parameters
+    ----------
+    mean : float
+        Mean of the Gaussian noise tensor.
+    std : float
+        Standard deviation of the noise tensor.
+    training : bool
+        Only apply Gaussian noise if True.
+    """
+    def __init__(self, mean, std, training=True):
+        self.mean = mean
+        self.std = std
+        self.training = training
+
+    def __call__(self, tensor):
+        if self.training:
+            return gaussian_noise(tensor, self.mean, self.std)
+        else:
+            return tensor
+
+
+def gaussian_noise(tensor, mean, stddev):
+    """Apply Gaussian noise to an input tensor.
+
+    Parameters
+    ----------
+    mean : float
+        Mean of the Gaussian noise tensor.
+    std : float
+        Standard deviation of the noise tensor.
+    """
+    noise = Variable(tensor.data.new(tensor.size()).normal_(mean, std))
+    return tensor + noise
